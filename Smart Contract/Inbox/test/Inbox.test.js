3c2a84dd1cb72aaf10366fdd1f343e650caca21a
barramalik7 <malikbarra@gmail.com>
2024-01-01 20:35:38 +0700

    update Inbox test

--- Smart Contract/Inbox/test/Inbox.test.js
+++ Smart Contract/Inbox/test/Inbox.test.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const  {abi, evm} = require('../compile');
 
 let accounts;
 beforeEach(async () => {
@@ -10,6 +11,12 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     // Use one of the account to deploy contract
+    new web3.eth.Contract(abi)
+    .deploy({
+        data: evm.bytecode.object,
+        argument: ['Halo']
+    })
+    .send({from: accounts[0]})
 });
 
 describe('Inbox', () => {
